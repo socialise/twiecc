0c0ee0f3af782d5f9db0ea74b501e9e69adb3990
Soejima <hand36ideal@yahoo.co.jp>
2016-12-27 14:50:56 +0900

    詳細Activityの追加

---
+++ app/src/main/java/com/dai1pan/Detail/DetailTweetActivity.java
@@ -0,0 +1,15 @@
+package com.dai1pan.Detail;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dai1pan.R;
+
+public class DetailTweetActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail_tweet);
+	}
+}
