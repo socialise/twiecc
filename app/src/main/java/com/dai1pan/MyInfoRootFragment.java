f14f8c23333a97cfe4636a74cc2639ee1c97e8a1
YutaKohashi <yutakohashi@mypc.local>
2017-01-09 16:55:02 +0900

    tab1のフラグメントを追加 フラグメントに２つのフラグメントをネスト

---
+++ app/src/main/java/com/dai1pan/MyInfoRootFragment.java
@@ -0,0 +1,20 @@
+package com.dai1pan;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MyInfoRootFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_my_info_root, container, false);
+        return v;
+    }
+
+}
