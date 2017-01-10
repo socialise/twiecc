026985475d51d06310c0e5c71492a47c2374bc15
Ryota Shimomura <ryowasawasa@yahoo.co.jp>
2017-01-10 15:00:36 +0900

    フォローボタン追加とフォローアクティビティ作成

---
+++ app/src/main/java/com/dai1pan/FollowActivity.java
@@ -0,0 +1,21 @@
+package com.dai1pan;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+/**
+ * IE3A11　下村怜大
+ */
+
+public class FollowActivity {
+
+    _tw.createFriendship(["ユーザー名"orユーザーID]);
+    _tw.destroyFriendship(["ユーザー名"orユーザーID]);
+
+}
